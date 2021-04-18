5e2cbcc3dcd097c63f809158f2e7f92c6a7b10d7
Nikita Patel <nix1228@gmail.com>
2021-04-18 14:00:07 -0700

    adding data streaming nanodegree project

---
+++ producers/models/producer.py
@@ -0,0 +1,81 @@
+"""Producer base-class providing common utilites and functionality"""
+import logging
+import time
+
+
+from confluent_kafka import avro
+from confluent_kafka.admin import AdminClient, NewTopic
+from confluent_kafka.avro import AvroProducer, CachedSchemaRegistryClient
+
+logger = logging.getLogger(__name__)
+
+BROKER_URL = "PLAINTEXT://localhost:9092"
+SCHEMA_REGISTRY_URL = "http://localhost:8081"
+
+
+class Producer:
+    """Defines and provides common functionality amongst Producers"""
+
+    # Tracks existing topics across all Producer instances
+    existing_topics = set([])
+
+    def __init__(
+        self,
+        topic_name,
+        key_schema,
+        value_schema=None,
+        num_partitions=1,
+        num_replicas=1,
+    ):
+        """Initializes a Producer object with basic settings"""
+        self.topic_name = topic_name
+        self.key_schema = key_schema
+        self.value_schema = value_schema
+        self.num_partitions = num_partitions
+        self.num_replicas = num_replicas
+
+        #
+        #
+        # Configure the broker properties.
+        self.broker_properties = {
+            "bootstrap.servers":BROKER_URL,
+            "group.id"="topic_name",
+        }
+        #Create cached schema registry client
+        schema_registry=CachedSchemaRegistryClient(
+            SCHEMA_REGISTRY_URL)
+        # If the topic does not already exist, try to create it
+        if self.topic_name not in Producer.existing_topics:
+            self.create_topic()
+            Producer.existing_topics.add(self.topic_name)
+
+        # Configure the AvroProducer
+        self.producer = AvroProducer(self.broker_properties,
+                                     schema_registry=
+                                     schema_registry,
+                                     default_key_schema=
+                                     self.key_schema,
+                                     default_value_schema=
+                                     self.value_schema
+                                    )
+        
+
+    def create_topic(self):
+        """Creates the producer topic if it does not already exist"""
+        client = AdminClient(self.broker_properties)
+        client.create_topics([NewTopic(self.topic_name,
+                                       self.num_partitions)])
+        logger.info("topic creation kafka integration incomplete - skipping")
+
+    def time_millis(self):
+        return int(round(time.time() * 1000))
+
+    def close(self):
+        """Prepares the producer for exit by cleaning up the producer"""
+        if self.producer is not None:
+            self.producer.flush()
+        logger.info("producer close incomplete - skipping")
+
+    def time_millis(self):
+        """Use this function to get the key for Kafka Events"""
+        return int(round(time.time() * 1000))
